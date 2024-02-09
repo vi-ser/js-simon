a9df4a33cf8084a7a7010f9b5a56d8099f339bc5
vi_ser <vinceulisse@gmail.com>
2024-02-09 14:24:47 +0100

    calcolata la differenza in millisecondi tra il momento dell'esecuzione del programma e il giorno del countdown, ora bisogna convertire i millisecondi nelle varie unità di misura

--- js/script.js
+++ js/script.js
@@ -13,9 +13,12 @@ Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accom
 Buon divertimento a tutte e a tutti!
 */
 
-// creo la data desiderata
+// definisco la data desiderata
 const theBigDay = new Date("2024-02-12T09:30:00");
 console.log(theBigDay);
+
+// definisco la data di oggi
+const today = new Date();
 
 
 // determino i millisecondi in tutte le unità di misura
@@ -28,3 +31,15 @@ let days = hours * 24;
 
 // calcolo i millisecondi di 3 giorni (esempio)
 console.log(days * 3);
+
+
+let millisecondsToday = today.getTime();
+let millisecondsTheBigDay = theBigDay.getTime();
+
+console.log("Millisecondi data di oggi: ", millisecondsToday);
+console.log("Millisecondi data countdown: ", millisecondsTheBigDay);
+
+
+// calcolo i millisecondi di differenza tra il momento dell'esecuzione del programma e la data del countdown
+let timeLeft = millisecondsTheBigDay - millisecondsToday;
+console.log("Conto alla rovescia: ", timeLeft, " millisecondi");
